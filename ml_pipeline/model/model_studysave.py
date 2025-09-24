6782bf4b25211047e164a656018b4e592f26e74c
VsevolodLazebnyi <vllazebnyi@gmail.com>
2025-09-24 13:06:59 +0300

    update

--- ml_pipeline/model/model_studysave.py
+++ ml_pipeline/model/model_studysave.py
@@ -1,4 +1,3 @@
-# wb_checker/ml_pipeline/model/model_studysave.py
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
